39f3ddabd12c6851613774c71b83710891698413
Lauigi <glbrt.liu@gmail.com>
2025-12-25 16:06:46 -0500

    feat: add login form ui

--- apps/dashboard/src/components/Login.tsx
+++ apps/dashboard/src/components/Login.tsx
@@ -1,6 +1,13 @@
 import { useMutation } from '../hooks/useMutation';
 import { auth } from '@/lib/auth';
 import { Button } from '@workspace/ui/components/button';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+  CardContent,
+} from '@workspace/ui/components/card';
 
 export function Login() {
   const loginMutation = useMutation({
@@ -8,49 +15,54 @@ export function Login() {
   });
 
   return (
-    <div className="fixed inset-0 bg-white dark:bg-black flex items-start justify-center p-8">
-      <div className="bg-white dark:bg-gray-900 p-8 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold mb-4">Login</h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const formData = new FormData(e.target as HTMLFormElement);
-            loginMutation.mutate(formData.get('email') as string);
-          }}
-          className="space-y-4"
-        >
-          <div>
-            <label htmlFor="email" className="block text-xs">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="px-2 py-1 w-full rounded-sm border border-gray-500/20 bg-white dark:bg-gray-800"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="block text-xs">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="px-2 py-1 w-full rounded-sm border border-gray-500/20 bg-white dark:bg-gray-800"
-            />
-          </div>
-          <Button
-            type="submit"
-            className="uppercase"
-            variant="outline"
-            disabled={loginMutation.status === 'pending'}
+    <div className="flex items-center justify-center p-24">
+      <Card className="w-full sm:max-w-md">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const formData = new FormData(e.target as HTMLFormElement);
+              loginMutation.mutate(formData.get('email') as string);
+            }}
+            className="space-y-4"
           >
-            {loginMutation.status === 'pending' ? '...' : 'Login'}
-          </Button>
-        </form>
-      </div>
+            <div>
+              <label htmlFor="email" className="block text-xs">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="px-2 py-1 w-full rounded-sm border border-gray-500/20 bg-white dark:bg-gray-800"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block text-xs">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="px-2 py-1 w-full rounded-sm border border-gray-500/20 bg-white dark:bg-gray-800"
+              />
+            </div>
+            <Button
+              type="submit"
+              className="uppercase"
+              variant="outline"
+              disabled={loginMutation.status === 'pending'}
+            >
+              {loginMutation.status === 'pending' ? '...' : 'Login'}
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
     </div>
   );
 }
---
+++ apps/dashboard/src/routes/login.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { Login } from '~/components/Login';
+
+export const Route = createFileRoute('/login')({
+  beforeLoad: ({ context }) => {
+    if (context.user.isAuthenticated) {
+      throw redirect({
+        to: '/',
+      });
+    }
+  },
+  component: () => <Login />,
+});
