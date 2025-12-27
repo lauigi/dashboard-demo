cdec8aebfe408e888a8cbcb65ffbbc58fac8dd19
Lauigi <glbrt.liu@gmail.com>
2025-12-26 23:08:08 -0500

    feat(api): add models and API structure for article management
    - Created models for Article, Author, Tag, and related request/response structures.
    - Implemented serialization and deserialization functions for models.
    - Established API entry points in index files for easier imports.
    - Added runtime configuration and request handling for API interactions.
    - Configured TypeScript settings for the API package.

---
+++ packages/api/src/apis/index.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthedApi';
+export * from './DefaultApi';
---
+++ packages/api/src/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Article';
+export * from './ArticlePostRequest';
+export * from './ArticlePostRequestTagsInner';
+export * from './ArticlesGet200Response';
+export * from './Author';
+export * from './Tag';
+export * from './TagIdDelete200Response';
+export * from './TagPost401Response';
+export * from './TagsGet200Response';
